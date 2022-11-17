1eae9fd3b7d73ad03c3844adc673e9c0a0d02ddb
olkar <karolkozakiewicz6@gmail.com>
2022-11-17 22:56:21 +0100

    Create random number beetwen 1 and 3

--- funcionality.js
+++ funcionality.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+function getComputerChoise(){
+    let choise = '';
+    let randomNumber = getRandomInt(1,4); // 1 is inclusive,4 exclusive
+    console.log(randomNumber);
+}
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min)+min);
+}
